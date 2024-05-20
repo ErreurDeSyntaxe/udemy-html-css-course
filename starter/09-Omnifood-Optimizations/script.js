a52357e5b55aeba983aab63059b483e4cb223af2
Xavier Bertrand <150564614+ErreurDeSyntaxe@users.noreply.github.com>
2024-05-20 13:58:24 +0800

    Build archaic smooth scrolling

--- starter/09-Omnifood-Optimizations/script.js
+++ starter/09-Omnifood-Optimizations/script.js
@@ -10,6 +10,29 @@ btnNavEl.addEventListener('click', () => {
   headerEl.classList.toggle('nav-open');
 });
 
+// Enable smooth scrolling (archaic need)
+const allLinks = document.querySelectorAll('a:link');
+allLinks.forEach((link) => {
+  link.addEventListener('click', (event) => {
+    event.preventDefault();
+    const href = link.getAttribute('href');
+    console.log(href);
+
+    // Scroll back to top
+    if (href === '#') window.scrollTo({ top: 0, behavior: 'smooth' });
+
+    // Scroll to section
+    if (href !== '#' && href.startsWith('#')) {
+      const sectionEl = document.querySelector(href);
+      sectionEl.scrollIntoView({ behavior: 'smooth' });
+    }
+
+    // Close mobile navigation
+    if (link.classList.contains('main-nav-link'))
+      headerEl.classList.remove('nav-open');
+  });
+});
+
 ///////////////////////////////////////////////////////////
 // Fixing flexbox gap property missing in some Safari versions
 function checkFlexGap() {
